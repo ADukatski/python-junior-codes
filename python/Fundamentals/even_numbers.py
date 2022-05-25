4599dbb270754cd3bb0306f8b862996a9366bffc
Anthony <anthony.dukatski@gmail.com>
2022-05-25 17:00:07 +0300

    Delete python/Fundamentals directory

--- python/Fundamentals/even_numbers.py
+++
@@ -1,9 +0,0 @@
-number = int(input())
-
-for i in range(number):
-    n = int(input())
-    if n % 2 != 0:
-        print(f"{n} is odd!")
-        break
-else:
-    print("All numbers are even.")
--- python/Fundamentals/maximum_multiple.py
+++
@@ -1,7 +0,0 @@
-divisor = int(input())
-boundary = int(input())
-
-for i in range(boundary, divisor, - 1):
-    if i % divisor == 0 and i > 0 and i <= boundary:
-        print(i)
-        break
